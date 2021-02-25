b8da79319981ffdc759accb635e8bc38a7532523
David <david.anthony.lei@gmail.com>
2021-02-25 15:33:04 +1100

    add useless sw

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log('hello world');
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js');
+    console.log('registered sw.js');
+}
---
+++ sw.js
@@ -0,0 +1,1 @@
+console.log('i am a service worker');
